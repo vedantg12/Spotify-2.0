8470ca1a07ea3c54ea0b22ec497e0ed0ad132cce
Vedant Gupta <vedantgupta@Vedants-MBP.fios-router.home>
2022-01-12 21:46:09 -0500

    fourth commit

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -4,9 +4,9 @@ import { NextResponse } from "next/server";
 export async function middleware(req){
     const token = await getToken({req, secret: process.env.JWT_SECRET});
     const {pathname} = req.nextUrl;
-    if(token && pathname == "/login"){
-        return NextResponse.redirect("/")
-    }
+    // if(token && pathname == "/login"){
+    //     return NextResponse.redirect("/")
+    // }
     if (pathname.includes('/api/auth') || token){
         return NextResponse.next();
     }
